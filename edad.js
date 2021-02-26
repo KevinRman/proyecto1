16e8547fdb5bac0a8b4969caa8b7bb4de0f7f007
KevinRman <78990459+KevinRman@users.noreply.github.com>
2021-02-25 20:43:27 -0500

    calculate age

---
+++ edad.js
@@ -0,0 +1,5 @@
+alert("hola, voy a calcular tu edad en base a tu año de nacimiento");
+let anho_actual = 2021;
+let anho_nacimiento = prompt("¿cuál es tu a{o de nacimiento?");
+let edad = anho_actual - anho_nacimiento;
+alert("en base a los datos suministrados tu tienes" + edad + " años de edad");
