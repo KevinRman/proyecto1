36347c15ab16a4a257e6d8497317c18963e1f38a
KevinRman <78990459+KevinRman@users.noreply.github.com>
2021-02-25 20:47:45 -0500

    year of birth

--- anho_nacimiento.js
+++ anho_nacimiento.js
@@ -2,4 +2,4 @@ alert("hola, voy a calcular tu año de nacimiento en base a tu edad");
 let anho_actual = 2021;
 let edad = prompt("¿cuál es tu edad?");
 let anho_nacimiento = anho_actual - edad;
-alert("A usted le faltan " + tiempo_pension + " años para pensionarse");
+alert("el año de en que naciste es " + anho_nacimiento);
